ba8541be6c6ac3717da5e6adb2329c1bbc6a64f0
Kuldeep Yeware <kuldeepyeware31@gmail.com>
2023-04-19 11:45:08 +0530

    Some changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "gateway.pinata.cloud/ipfs",
+      "https://nft-marketplace-sage-nine.vercel.app",
+    ],
+  },
 };
 
 module.exports = nextConfig;
--- setupProxy.js
+++ setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/pinning",
+    "/",
     createProxyMiddleware({
-      target: "https://api.pinata.cloud",
+      target: "https://gateway.pinata.cloud/ipfs",
       changeOrigin: true,
     })
   );
