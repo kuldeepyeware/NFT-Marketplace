8782fbd877d33937b345cf35b14dc017589d0923
Kuldeep Yeware <kuldeepyeware31@gmail.com>
2023-04-19 11:47:40 +0530

    Some changes

--- setupProxy.js
+++ setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/",
+    "/pinning",
     createProxyMiddleware({
       target: "https://gateway.pinata.cloud/ipfs",
       changeOrigin: true,
